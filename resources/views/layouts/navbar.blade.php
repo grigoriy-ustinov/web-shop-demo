0c4619a6cd395e524ae117108a9f2ac6831fb127
Grigoriy.U <grisha@telia.com>
2018-08-02 16:08:56 +0200

    checkout button -> shopping cart

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
     <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
                 <li class="{{ Request::path() == 'shop' ? 'active' : '' }}"><a href="/shop">Shop</a></li>
-                <li class="{{ Request::path() == 'checkout' ? 'active' : '' }}"><a href="/checkout">Checkout <span class="badge">{{ (Session::has('cart') && Session::get('cart')->totalQty != 0)? Session::get('cart')->totalQty : ''}}</span></a></a></li>
+                <li class="{{ Request::path() == 'checkout' ? 'active' : '' }}"><a href="/checkout">Shopping cart <span class="badge">{{ (Session::has('cart') && Session::get('cart')->totalQty != 0)? Session::get('cart')->totalQty : ''}}</span></a></a></li>
                 <li class="{{ (Request::path() == 'adminView' || Request::path() == 'login') ? 'active' : '' }}"><a href="/admin">Admin</a></li>
         </ul>
     </div>
